bf816df5857db3242a20880e6eb9a543617086c0
Brennan McDonald <Brennan.mcdonald.mail@gmail.com>
2017-05-08 21:04:46 -0400

    Finished Character class, created Scanner class

--- character.go
+++ character.go
@@ -1,4 +1,6 @@
-/* 
+package main
+
+/*
  * Character class, encompases and individual character.
  * Holds:
  * - One Character         self.Cargo
@@ -7,18 +9,43 @@
  * - Character's column    self.Column
  * - Reference to source   self.Source
  */
-class Character {
-   public Cargo  char;
-   public Index  int;
-   public Line   int;
-   public Column int;
-   public Source &string;
 
-   func Character(c char, i int, l int, col int, source &string){
-      this.Cargo = c;
-      this.Index = i;
-      this.Line = l;
-      this.Column = col;
-      this.Source = source;
-   }
+import (
+   "fmt"
+)
+
+type Character struct {
+   Cargo  rune;
+   Index  int;
+   Line   int;
+   Column int;
+   Source *string;
 }
+
+func (c *Character) getCargo() *rune { return &c.Cargo }
+func (c *Character) getIndex() *int { return &c.Index }
+func (c *Character) getLine() *int { return &c.Line }
+func (c *Character) getColumn() *int { return &c.Column }
+
+func (c *Character) String() string {
+   var cargo string;
+   if (c.Cargo == ' '){
+      cargo = "   Space";
+   } else if (c.Cargo == '\n'){
+      cargo = "   NewLine";
+   } else if (c.Cargo == '\t'){
+      cargo = "   Tab";
+   } else if (int(c.Cargo) < 1){
+      cargo = "   Endline";
+   } else {
+      cargo = string(c.Cargo);
+   }
+   return fmt.Sprintf("%d\t%d %d\t%s", c.Index, c.Line, c.Column, cargo);
+}
+/*
+func main(){
+   text := "asdfasd"
+   c := Character{'\n', 1, 1, 1, &text};
+   fmt.Println(c.String());
+}
+*/
